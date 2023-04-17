765f573e3904bf231e5f75fee1a622decad7658f
young <yyxdev@gmail.com>
2023-04-17 18:26:45 +0800

    //

--- ObjcProvingground/ViewController.m
+++ ObjcProvingground/ViewController.m
@@ -17,23 +17,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.btn1 setTitle:@"1" forState:UIControlStateNormal];
-    [self.btn2 setTitle:@"2" forState:UIControlStateNormal];
+  
     // Do any additional setup after loading the view.
 }
 - (IBAction)clickBtn1:(id)sender {
-    [self.btn1 setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
-    [self.btn2 setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-//    [self.btn1 setTitle:@"3" forState:UIControlStateNormal];
+  
 //    [self.btn2 setTitle:@"4" forState:UIControlStateNormal];
 
 
 }
 - (IBAction)clickBtn2:(id)sender {
-    [self.btn1 setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
-    [self.btn2 setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
-//    [self.btn1 setTitle:@"1" forState:UIControlStateNormal];
-//    [self.btn2 setTitle:@"2" forState:UIControlStateNormal];
+   
 }
 
 
