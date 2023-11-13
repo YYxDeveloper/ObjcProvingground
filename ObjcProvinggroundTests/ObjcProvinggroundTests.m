ec6124f0bba9453c698b82aa6199bb3978312727
young <yyxdev@gmail.com>
2023-11-13 13:30:53 +0800

    setting test target

--- ObjcProvinggroundTests/ObjcProvinggroundTests.m
+++ ObjcProvinggroundTests/ObjcProvinggroundTests.m
@@ -21,16 +21,7 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+-(void)testCheckEnterPriceGreater10PGP{
+    assert(YES);
 }
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
